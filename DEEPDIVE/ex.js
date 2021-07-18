9285efc344d017d3d532db74755301f6ee907a78
이준희 <junh0328@naver.com>
2021-07-18 14:34:32 +0900

    DEEP DIVE 15장 정리

--- DEEPDIVE/ex.js
+++ DEEPDIVE/ex.js
@@ -1,8 +1,20 @@
-function foo() {
-  var x = "local";
-  console.log(x); // local
-  return x;
-}
+// console.log("var keyword declaration: ", variable);
+// // console.log("let keyword declaration: ", foo); // Cannot access 'foo' before initialization
+// // console.log("const keyword declaration: ", FOO); // Cannot access 'FOO' before initialization
 
-foo();
-// console.log(x);
+// var variable = 6;
+// console.log("variable after initialized: ", variable);
+
+// let foo = 7;
+// console.log("foo after initialized: ", foo);
+
+// const FOO = 8;
+// console.log("FOO after initialized: ", FOO);
+
+let foo = 1; // 전역 변수
+
+// 블록 레벨 스코프의 특징을 갖는 let 키워드
+{
+  console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
+  let foo = 2; // 지역 변수
+}
