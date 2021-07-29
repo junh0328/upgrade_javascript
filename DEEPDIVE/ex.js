be37c8072b8fc940e4f40c09daec6e16d3c7a14c
이준희 <junh0328@naver.com>
2021-07-29 13:55:59 +0900

    DEEP DIVE ~20장까지 재 정리 및 내용 추가

--- DEEPDIVE/ex.js
+++ DEEPDIVE/ex.js
@@ -1,15 +1,6 @@
-// 외부 함수(outer function)
-function outer() {
-  var x = 1;
-
-  // 중첩 함수(nested function), 내부 함수(inner function)
-  function inner() {
-    var y = 2;
-    // 외부 함수의 변수를 참조할 수 있다.
-    console.log(x + y); // 3
-  }
-
-  inner();
+function foo() {
+  let x = 10;
 }
+foo();
 
-outer();
+console.log(x); // ?
